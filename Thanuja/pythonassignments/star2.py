2e50696b2fee442f9ce8791b2916d687cbb5f1c2
sajjathanuja <sajjathanuja29@gmail.com>
2022-12-11 10:05:21 -0800

    adding all python assignment scripts

---
+++ Thanuja/pythonassignments/star2.py
@@ -0,0 +1,13 @@
+for i in range(5,0,-1):
+    star=""
+    for j in range(0,i):
+        star=""+"*"
+        print(star)
+        for k in range(0,5):
+            line=k*"*"
+            print(line)
+
+
+
+
+
