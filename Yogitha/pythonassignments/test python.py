557a46843a463bd816d9b93d63571b16c54213b2
yogik2 <yogitha.kota@gmail.com>
2022-12-28 00:15:57 -0500

    updated file

--- Yogitha/pythonassignments/test python.py
+++ Yogitha/pythonassignments/test python.py
@@ -1,11 +1,17 @@
 import sys
 
-file_name = sys.argv[1]
+file_name = sys.argv[0]
+file_operation = sys.argv[1]
+file_content = sys.argv[2]
 
-fileobject = open(file_name,"r")
-for eachline in fileobject:
-    eachline = eachline.rstrip()
-    print(eachline)
-    fileobject.close()
+fileobject = open(file_name, file_operation)
+if file_operation == "r":
+    for eachline in fileobject:
+        eachline = eachline.rstrip()
+        print(eachline)
+elif file_operation == "w":
+    fileobject.write(file_content)
+else:
+    print("please enter valid file operation, it must be r/w")
 
-
+fileobject.close()
