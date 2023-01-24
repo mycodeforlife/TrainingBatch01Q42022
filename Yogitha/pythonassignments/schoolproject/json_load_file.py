317ba492a64101ccd1323b06e9dad88f63c68ba0
yogik2 <yogitha.kota@gmail.com>
2023-01-23 23:40:18 -0500

    json_dump_files.py

--- Yogitha/pythonassignments/schoolproject/json_load_file.py
+++
@@ -1,9 +0,0 @@
-import json
-from io import StringIO
-str_io_obj = StringIO('["Ukraine", "United Kingdom", "Ukraine", "United States"]')
-
-#load from StringIO
-
-print('Load: ' + str(json.load(str_io_obj)))
-print('String to Json: ' + str(json.loads
-                ('{"Ukraine" : 100, "United Kingdom" : 102, "Ukraine" : 100, "United States" : 103}')))
