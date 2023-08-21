eb3c43cfbafc7ce45bfd73736077f937c257e956
ponchitra90 <pcchitrapc@gmail.com>
2023-08-21 14:24:09 -0500

    diamond pattern

--- Chithra/pythonassignments/star pattern.py
+++ Chithra/pythonassignments/star pattern.py
@@ -2,7 +2,7 @@
 this script will generate * tree for different patterns
 
 """
-
+"""
 n=int(input("Please enter the n value:"))
 for i in range(0,n):
     j = (n-i-1)*" " + i*"*"
@@ -38,3 +38,35 @@ for i in range(0,n):
 for k in range (n+1,n+3):
     print("  " + "*")
 print()
+
+"""
+
+n=int(input("Please enter the n value:"))
+for i in range(0,n):
+    for j in range(n-i,0,-1):
+        print("*",end=" ")
+    print()
+
+n=int(input("Please enter the n value:"))
+for i in range(0,n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
+
+n = int(input("Please enter the n value:"))
+for i in range(0, n-1):
+    for j in range(n-i,0,-1):
+        print(" ", end=" ")
+    for j in range(i):
+        print("*",end=" ")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(0,n):
+    for j in range(i+1):
+        print(" ", end=" ")
+    for j in range(i,n-1):
+        print("*",end=" ")
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
