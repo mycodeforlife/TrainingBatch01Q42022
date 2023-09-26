4a3feace08ac70530f9d0a5a91bde9f3cef1b1ad
ponchitra90 <pcchitrapc@gmail.com>
2023-09-26 11:49:49 -0500

    Web Connect

--- Chithra/pythonassignments/star pattern.py
+++ Chithra/pythonassignments/star pattern.py
@@ -41,32 +41,34 @@ print()
 
 """
 
-n=int(input("Please enter the n value:"))
-for i in range(0,n):
-    for j in range(n-i,0,-1):
-        print("*",end=" ")
-    print()
+# n=int(input("Please enter the n value:"))
+# for i in range(0,n):
+#     for j in range(n-i,0,-1):
+#         print("*",end=" ")
+#     print()
+#
+# n=int(input("Please enter the n value:"))
+# for i in range(0,n):
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     print()
+#
+# n = int(input("Please enter the n value:"))
+# for i in range(0, n-1):
+#     for j in range(n-i,0,-1):
+#         print(" ", end=" ")
+#     for j in range(i):
+#         print("*",end=" ")
+#     for j in range(i+1):
+#         print("*",end=" ")
+#     print()
+# for i in range(0,n):
+#     for j in range(i+1):
+#         print(" ", end=" ")
+#     for j in range(i,n-1):
+#         print("*",end=" ")
+#     for j in range(i,n):
+#         print("*",end=" ")
+#     print()
 
-n=int(input("Please enter the n value:"))
-for i in range(0,n):
-    for j in range(i,n):
-        print("*",end=" ")
-    print()
 
-n = int(input("Please enter the n value:"))
-for i in range(0, n-1):
-    for j in range(n-i,0,-1):
-        print(" ", end=" ")
-    for j in range(i):
-        print("*",end=" ")
-    for j in range(i+1):
-        print("*",end=" ")
-    print()
-for i in range(0,n):
-    for j in range(i+1):
-        print(" ", end=" ")
-    for j in range(i,n-1):
-        print("*",end=" ")
-    for j in range(i,n):
-        print("*",end=" ")
-    print()
